cde03a2ad7533ea6ce940d365bb8cf59eb19b7a6
Manuel Pichler <mapi@manuel-pichler.de>
2008-02-17 15:14:00 +0000

    - containsCycles() method added to framework facade.
    git-svn-id: svn://xplib.de/PHP_Depend@77 b9671ce7-7109-4c08-962d-a4a26ea92831

---
+++ trunk/tests/PHP/Depend/data/code-without-cycle/code.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package package0
+ */
+class clazz0
+{
+    public function __construct()
+    {
+        clazz1::foo();
+    }
+}
+
+/**
+ * @package package1
+ */
+class clazz1 {
+    
+}
