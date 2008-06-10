8ce08cc24d188d8fa77a65f54acbe93d11e875eb
Manuel Pichler <mapi@manuel-pichler.de>
2008-06-10 14:54:37 +0000

    - New coupling analyzer implemented.
    git-svn-id: svn://xplib.de/PHP_Depend@245 b9671ce7-7109-4c08-962d-a4a26ea92831

---
+++ trunk/PHP/Depend/Metrics/Coupling/Analyzer.php
@@ -0,0 +1,268 @@
+<?php
+/**
+ * This file is part of PHP_Depend.
+ * 
+ * PHP Version 5
+ *
+ * Copyright (c) 2008, Manuel Pichler <mapi@pmanuel-pichler.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage Metrics
+ * @author     Manuel Pichler <mapi@manuel-pichler.de>
+ * @copyright  2008 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://www.manuel-pichler.de/
+ */
+
+require_once 'PHP/Depend/Code/NodeVisitor/AbstractDefaultVisitor.php';
+require_once 'PHP/Depend/Metrics/AnalyzerI.php';
+require_once 'PHP/Depend/Metrics/ProjectAwareI.php';
+
+/**
+ * This analyzer collects coupling values for the hole project. It calculates 
+ * all function and method <b>calls</b> and the <b>fanout</b>, that means the 
+ * number of referenced types.
+ * 
+ * The FANOUT calculation is based on the definition used by the apache maven
+ * project.
+ * 
+ * <ul>
+ *   <li>field declarations (Uses doc comment annotations)</li>
+ *   <li>formal parameters and return types (The return type uses doc comment
+ *   annotations)</li>
+ *   <li>throws declarations (Uses doc comment annotations)</li>
+ *   <li>local variables</li>
+ * </ul>
+ * 
+ * http://www.jajakarta.org/turbine/en/turbine/maven/reference/metrics.html#Fan%20Out%20-%20FANOUT
+ * 
+ * The implemented algorithm counts each type only once for a method and function.
+ * Any type that is either a supertype or a subtype of the class is not counted.  
+ *
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage Metrics
+ * @author     Manuel Pichler <mapi@manuel-pichler.de>
+ * @copyright  2008 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://www.manuel-pichler.de/
+ */
+class PHP_Depend_Metrics_Coupling_Analyzer
+       extends PHP_Depend_Code_NodeVisitor_AbstractDefaultVisitor
+    implements PHP_Depend_Metrics_AnalyzerI,
+               PHP_Depend_Metrics_ProjectAwareI
+{
+    /**
+     * The number of method or function calls.
+     *
+     * @type integer
+     * @var integer $_calls
+     */
+    private $_calls = -1;
+    
+    /**
+     * Number of fanouts.
+     *
+     * @type integer
+     * @var integer $_fanout
+     */
+    private $_fanout = -1;
+    
+    /**
+     * Provides the project summary as an <b>array</b>.
+     * 
+     * <code>
+     * array(
+     *     'calls'   =>  23,
+     *     'fanout'  =>  42
+     * )
+     * </code>
+     *
+     * @return array(string=>mixed)
+     */
+    public function getProjectMetrics()
+    {
+        return array(
+            'calls'   =>  $this->_calls,
+            'fanout'  =>  $this->_fanout
+        );
+    }
+    
+    /**
+     * Processes all {@link PHP_Depend_Code_Package} code nodes.
+     *
+     * @param PHP_Depend_Code_NodeIterator $packages All code packages.
+     * 
+     * @return void
+     */
+    public function analyze(PHP_Depend_Code_NodeIterator $packages)
+    {
+        // Check for previous run
+        if ($this->_calls === -1) {
+            // Init metrics
+            $this->_calls  = 0;
+            $this->_fanout = 0;
+            
+            // Process all packages
+            foreach ($packages as $package) {
+                $package->accept($this);
+            }
+        }
+    }
+    
+    /**
+     * Visits a function node. 
+     *
+     * @param PHP_Depend_Code_Function $function The current function node.
+     * 
+     * @return void
+     * @see PHP_Depend_Code_NodeVisitorI::visitFunction()
+     */
+    public function visitFunction(PHP_Depend_Code_Function $function)
+    {
+        $fanouts = array();
+        if (($type = $function->getReturnType()) !== null) {
+            $fanouts[] = $type;
+            ++$this->_fanout;
+        }
+        foreach ($function->getExceptionTypes() as $type) {
+            if (in_array($type, $fanouts, true) === false) {
+                $fanouts[] = $type;
+                ++$this->_fanout;
+            }
+        }
+        foreach ($function->getDependencies() as $type) {
+            if (in_array($type, $fanouts, true) === false) {
+                ++$this->_fanout;
+            }
+        }
+        
+        $this->_countCalls($function);
+    }
+    
+    /**
+     * Visits a method node. 
+     *
+     * @param PHP_Depend_Code_Class $method The method class node.
+     * 
+     * @return void
+     * @see PHP_Depend_Code_NodeVisitorI::visitMethod()
+     */
+    public function visitMethod(PHP_Depend_Code_Method $method)
+    {
+        $parent = $method->getParent();
+        
+        $fanouts = array();
+        if (($type = $method->getReturnType()) !== null) {
+            if (!$type->isSubtypeOf($parent) && !$parent->isSubtypeOf($type)) {
+                $fanouts[] = $type;
+                ++$this->_fanout;
+            }
+        }
+        foreach ($method->getExceptionTypes() as $type) {
+            if (in_array($type, $fanouts, true) === true) {
+                continue;
+            }
+            if (!$type->isSubtypeOf($parent) && !$parent->isSubtypeOf($type)) {
+                $fanouts[] = $type;
+                ++$this->_fanout;
+            }
+        }
+        foreach ($method->getDependencies() as $type) {
+            if (in_array($type, $fanouts, true) === true) {
+                continue;
+            }
+            if (!$type->isSubtypeOf($parent) && !$parent->isSubtypeOf($type)) {
+                $fanouts[] = $type;
+                ++$this->_fanout;
+            }
+        }
+        
+        $this->_countCalls($method);
+    }
+    
+    /**
+     * Visits a property node. 
+     *
+     * @param PHP_Depend_Code_Property $property The property class node.
+     * 
+     * @return void
+     * @see PHP_Depend_Code_NodeVisitorI::visitProperty()
+     */
+    public function visitProperty(PHP_Depend_Code_Property $property)
+    {
+        // Check for not null
+        if (($type = $property->getType()) === null) {
+            return;
+        }
+        
+        $parent = $property->getParent();
+        
+        // Only increment if these types are not part of the same hierarchy
+        if (!$type->isSubtypeOf($parent) && !$parent->isSubtypeOf($type)) {
+            ++$this->_fanout;
+        }
+    }
+    
+    /**
+     * Counts all calls within the given <b>$callable</b>
+     *
+     * @param PHP_Depend_Code_AbstractCallable $callable Context callable.
+     * 
+     * @return void
+     */
+    private function _countCalls(PHP_Depend_Code_AbstractCallable $callable)
+    {
+        $tokenTypes = array(
+            PHP_Depend_Code_TokenizerI::T_STRING,
+            PHP_Depend_Code_TokenizerI::T_VARIABLE
+        );
+        
+        $prevToken1 = null;
+        $prevToken2 = null;
+        foreach ($callable->getTokens() as $token) {
+            if ($token[0] === PHP_Depend_Code_TokenizerI::T_PARENTHESIS_OPEN
+             && $prevToken2 !== PHP_Depend_Code_TokenizerI::T_NEW
+             && in_array($prevToken1, $tokenTypes, true) === true) {
+                
+                ++$this->_calls;
+            }
+            
+            $prevToken2 = $prevToken1;
+            $prevToken1 = $token[0];
+        }
+    }
+}
