8c9f1c2af2db9fd826aff18208e993c166138f0b
Manuel Pichler <mapi@manuel-pichler.de>
2008-09-26 21:16:54 +0000

    - Initial commit for the native reflection sub project.
    git-svn-id: svn://pdepend.org/PHP_Depend/trunk@370 b9671ce7-7109-4c08-962d-a4a26ea92831

---
+++ tests/PHP/Reflection/Ast/IteratorTest.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * This file is part of PHP_Reflection.
+ * 
+ * PHP Version 5
+ *
+ * Copyright (c) 2008, Manuel Pichler <mapi@pdepend.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   PHP
+ * @package    PHP_Reflection
+ * @subpackage Ast
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://www.manuel-pichler.de/
+ */
+
+require_once dirname(__FILE__) . '/../AbstractTest.php';
+require_once dirname(__FILE__) . '/_dummy/TestImplNode.php';
+
+require_once 'PHP/Reflection/Ast/Iterator.php';
+require_once 'PHP/Reflection/Ast/Class.php';
+require_once 'PHP/Reflection/Ast/Function.php';
+require_once 'PHP/Reflection/Ast/Method.php';
+require_once 'PHP/Reflection/Ast/Package.php';
+
+/**
+ * Test case the node iterator.
+ *
+ * @category   PHP
+ * @package    PHP_Reflection
+ * @subpackage Ast
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://www.manuel-pichler.de/
+ */
+class PHP_Reflection_Ast_IteratorTest extends PHP_Reflection_AbstractTest
+{
+    /**
+     * Tests the ctor with an invalid input array which must result in an exception.
+     *
+     * @return void
+     */
+    public function testCreateIteratorWithInvalidInputFail()
+    {
+        $this->setExpectedException('RuntimeException');
+        new PHP_Reflection_Ast_Iterator(array(new stdClass()));
+    }
+    
+    /**
+     * Tests the ctor with an valid input array of {@link PHP_Reflection_Ast_NodeI}
+     * objects.
+     *
+     * @return void
+     */
+    public function testCreateIteratorWithValidInput()
+    {
+        $nodes = array(
+            new PHP_Reflection_Ast_TestImplNode('clazz'),
+            new PHP_Reflection_Ast_TestImplNode('pkg'),
+            new PHP_Reflection_Ast_TestImplNode('method'),
+            new PHP_Reflection_Ast_TestImplNode('func'),
+        );
+        
+        $it = new PHP_Reflection_Ast_Iterator($nodes);
+        
+        $this->assertEquals(4, $it->count());
+    }
+    
+    /**
+     * Tests that the iterator returns the node name as key.
+     *
+     * @return void
+     */
+    public function testIteratorLoopWithKey()
+    {
+        $names = array('clazz', 'func', 'method', 'pkg');
+        $nodes = array(
+            new PHP_Reflection_Ast_TestImplNode('clazz'),
+            new PHP_Reflection_Ast_TestImplNode('func'),
+            new PHP_Reflection_Ast_TestImplNode('method'),
+            new PHP_Reflection_Ast_TestImplNode('pkg'),
+        );
+        
+        $it = new PHP_Reflection_Ast_Iterator($nodes);
+        
+        for ($i = 0, $it->rewind(); $it->valid(); ++$i, $it->next()) {
+            $this->assertSame($nodes[$i], $it->current());
+            $this->assertEquals($names[$i], $it->key());
+        }
+    }
+}
---
+++ tests/PHP/Reflection/Visitor/_dummy/TestImplAbstractVisitor.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * This file is part of PHP_Reflection.
+ * 
+ * PHP Version 5
+ *
+ * Copyright (c) 2008, Manuel Pichler <mapi@pdepend.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   PHP
+ * @package    PHP_Reflection
+ * @subpackage Visitor
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://www.manuel-pichler.de/
+ */
+
+require_once 'PHP/Reflection/Visitor/AbstractVisitor.php';
+
+/**
+ * Dummy implementation of the default visitor.
+ *
+ * @category   PHP
+ * @package    PHP_Reflection
+ * @subpackage Visitor
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://www.manuel-pichler.de/
+ */
+class PHP_Reflection_Visitor_TestImplAbstractVisitor extends PHP_Reflection_Visitor_AbstractVisitor
+{
+    /**
+     * Collected visit order.
+     *
+     * @type array<integer>
+     * @var array(string=>integer)
+     */
+    public $visits = array();
+    
+    /**
+     * Visits a class node. 
+     *
+     * @param PHP_Reflection_Ast_Class $class The current class node.
+     * 
+     * @return void
+     * @see PHP_Reflection_VisitorI::visitClass()
+     */
+    public function visitClass(PHP_Reflection_Ast_Class $class)
+    {
+        $this->visits[] = $class->getName();
+        
+        parent::visitClass($class);
+    }
+    
+    /**
+     * Visits a file node. 
+     *
+     * @param PHP_Reflection_Ast_File $file The current file node.
+     * 
+     * @return void
+     * @see PHP_Reflection_VisitorI::visitFile()
+     */
+    public function visitFile(PHP_Reflection_Ast_File $file)
+    {
+        $this->visits[] = $file->getName();
+        
+        parent::visitFile($file);
+    }
+    
+    /**
+     * Visits a function node. 
+     *
+     * @param PHP_Reflection_Ast_Function $function The current function node.
+     * 
+     * @return void
+     * @see PHP_Reflection_VisitorI::visitFunction()
+     */
+    public function visitFunction(PHP_Reflection_Ast_Function $function)
+    {
+        $this->visits[] = $function->getName();
+        
+        parent::visitFunction($function);
+    }
+    
+    /**
+     * Visits a code interface object.
+     *
+     * @param PHP_Reflection_Ast_Interface $interface The context code interface.
+     * 
+     * @return void
+     * @see PHP_Reflection_VisitorI::visitInterface()
+     */
+    public function visitInterface(PHP_Reflection_Ast_Interface $interface)
+    {
+        $this->visits[] = $interface->getName();
+        
+        parent::visitInterface($interface);
+    }
+    
+    /**
+     * Visits a method node. 
+     *
+     * @param PHP_Reflection_Ast_Class $method The method class node.
+     * 
+     * @return void
+     * @see PHP_Reflection_VisitorI::visitMethod()
+     */
+    public function visitMethod(PHP_Reflection_Ast_Method $method)
+    {
+        $this->visits[] = $method->getName();
+        
+        parent::visitMethod($method);
+    }
+    
+    /**
+     * Visits a package node. 
+     *
+     * @param PHP_Reflection_Ast_Class $package The package class node.
+     * 
+     * @return void
+     * @see PHP_Reflection_VisitorI::visitPackage()
+     */
+    public function visitPackage(PHP_Reflection_Ast_Package $package)
+    {
+        $this->visits[] = $package->getName();
+        
+        parent::visitPackage($package);
+    }
+    
+    /**
+     * Visits a property node. 
+     *
+     * @param PHP_Reflection_Ast_Property $property The property class node.
+     * 
+     * @return void
+     * @see PHP_Reflection_VisitorI::visitProperty()
+     */
+    public function visitProperty(PHP_Reflection_Ast_Property $property)
+    {
+        $this->visits[] = $property->getName();
+        
+        parent::visitProperty($property);
+    }
+}
