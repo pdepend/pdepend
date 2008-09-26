63211292a9defca19035a26360a707a3fc254ae9
Manuel Pichler <mapi@manuel-pichler.de>
2008-09-26 21:16:54 +0000

    - Initial commit for the native reflection sub project.
    git-svn-id: svn://xplib.de/PHP_Depend@370 b9671ce7-7109-4c08-962d-a4a26ea92831

---
+++ trunk/tests/PHP/Reflection/Input/ExcludePathFilterTest.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * This file is part of PHP_Reflection.
+ * 
+ * PHP Version 5
+ *
+ * Copyright (c) 2008, Manuel Pichler <mapi@pdepend.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   PHP
+ * @package    PHP_Reflection
+ * @subpackage Input
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://www.manuel-pichler.de/
+ */
+
+require_once dirname(__FILE__) . '/../AbstractTest.php';
+
+require_once 'PHP/Reflection/Input/ExcludePathFilter.php';
+
+/**
+ * Test case for the exclude path filter.
+ *
+ * @category   PHP
+ * @package    PHP_Reflection
+ * @subpackage Input
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://www.manuel-pichler.de/
+ */
+class PHP_Reflection_Input_ExcludePathFilterTest extends PHP_Reflection_AbstractTest
+{
+    /**
+     * Tests the exclude filter with a single file in the exclude list.
+     *
+     * @return void
+     */
+    public function testExcludePathFilterWithFile()
+    {
+        $files  = array('input/exclude-path/file_j.php');
+        $filter = new PHP_Reflection_Input_ExcludePathFilter($files);
+        
+        $it = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(
+                self::createResourceURI('input/exclude-path')
+            )
+        );
+        
+        $expected = array(
+            'file_k.php'
+        );
+        
+        $actual = array();
+        foreach ($it as $file) {
+            if ($file->isDir() === false && $filter->accept($file)) {
+                $actual[] = $file->getFilename();
+            }
+        }
+        
+        sort($expected);
+        sort($actual);
+        
+        $this->assertEquals($expected, $actual);
+    }
+    
+    /**
+     * Tests the path exclude filter with a directory in the exclude list.
+     *
+     * @return void
+     */
+    public function testExcludePathFilterWithDirectory()
+    {
+        $files  = array('input/exclude-path');
+        $filter = new PHP_Reflection_Input_ExcludePathFilter($files);
+        
+        $it = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(self::createResourceURI('input'))
+        );
+        
+        $expected = array(
+            'file_a.php',
+            'file_b.php',
+            'file_c.php',
+            'file_d.phtml',
+            'file_e.phtml',
+            'file_f.phtml',
+            'file_g.inc',
+            'file_h.inc',
+            'file_i.inc',
+        );
+        
+        $actual = array();
+        foreach ($it as $file) {
+            if ($file->isDir() === false && $filter->accept($file)) {
+                $actual[] = $file->getFilename();
+            }
+        }
+        
+        sort($expected);
+        sort($actual);
+        
+        $this->assertEquals($expected, $actual);
+    }
+    
+    /**
+     * Tests the path exclude filter with a mix of files and directories.
+     *
+     * @return void
+     */
+    public function testExcludePathFilterWithFileAndDirectory()
+    {
+        $filter = new PHP_Reflection_Input_ExcludePathFilter(
+            array(
+                '/exclude-path/',
+                '/*.php',
+                '/*.inc',
+            )
+        );
+            
+        $it = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(self::createResourceURI('input'))
+        );
+        
+        $expected = array(
+            'file_d.phtml',
+            'file_e.phtml',
+            'file_f.phtml',
+        );
+        
+        $actual = array();
+        foreach ($it as $file) {
+            if ($file->isDir() === false && $filter->accept($file)) {
+                $actual[] = $file->getFilename();
+            }
+        }
+        
+        sort($expected);
+        sort($actual);
+        
+        $this->assertEquals($expected, $actual);
+    }
+}
