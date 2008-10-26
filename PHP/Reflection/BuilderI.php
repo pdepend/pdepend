1a29868361487494a3ae699bd61febb28aac0ecc
Manuel Pichler <mapi@manuel-pichler.de>
2008-10-26 13:03:48 +0000

    - '+global' package renamed to '+unknown'
    git-svn-id: svn://xplib.de/PHP_Depend/trunk@473 b9671ce7-7109-4c08-962d-a4a26ea92831

--- PHP/Reflection/BuilderI.php
+++ PHP/Reflection/BuilderI.php
@@ -61,7 +61,7 @@ interface PHP_Reflection_BuilderI extends IteratorAggregate
     /**
      * The default package name.
      */
-    const UNKNOWN_PKG = '+global';
+    const UNKNOWN_PKG = '+unknown';
     
     /**
      * Generic build class for classes and interfaces. This method should be used
