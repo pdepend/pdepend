2594c4309c12a0006b710ceeab4a6ad83973b0ce
Manuel Pichler <mapi@manuel-pichler.de>
2008-10-29 18:08:06 +0000

    - More methods for PHP's reflection wrapper implemented and tested.
    git-svn-id: svn://xplib.de/PHP_Depend@491 b9671ce7-7109-4c08-962d-a4a26ea92831

---
+++ trunk/tests/PHP/Reflection/_code/wrapper/method/number_of_params_4.php
@@ -0,0 +1,5 @@
+<?php
+class test_wrapper_method_number_of_params_4 {
+    public function test($x = 1, $y = 3.14, $z = null, $a) {}
+}
+?>
---
+++ trunk/tests/PHP/Reflection/_code/wrapper/method/returns_reference.php
@@ -0,0 +1,5 @@
+<?php
+class test_wrapper_method_returns_reference {
+    public function &test() {}
+}
+?>
