9684709800d19af06b953cfd3f6d461c48858261
Manuel Pichler <mapi@manuel-pichler.de>
2008-11-10 19:56:02 +0000

    - Additional statement tests added.
    git-svn-id: svn://pdepend.org/PHP_Depend/trunk@520 b9671ce7-7109-4c08-962d-a4a26ea92831

---
+++ tests/PHP/Reflection/_code/parser/statements/if_else_with_statements.php
@@ -0,0 +1,9 @@
+<?php
+function foo() {
+    if (true || false) {
+        echo "TRUE or FALSE";
+    } else {
+        echo "not TRUE and FALSE";
+    }
+}
+?>
---
+++ tests/PHP/Reflection/_code/parser/statements/if_elseif_else_with_statements.php
@@ -0,0 +1,10 @@
+<?php
+function foo() {
+    if (true)
+        echo "if";
+    elseif (false)
+        echo "elseif";
+    else
+        echo "else";
+}
+?>
