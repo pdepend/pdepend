0d24772e3442de0ebba3ce698b416c965abdc11a
Manuel Pichler <mapi@manuel-pichler.de>
2008-12-26 22:37:57 +0000

    - Unnecessary code removed, what brings a relevant performance impact.
    git-svn-id: svn://pdepend.org/PHP_Depend/branches/0.9.0@587 b9671ce7-7109-4c08-962d-a4a26ea92831

--- tests/PHP/Depend/Code/Filter/PackageTest.php
+++ tests/PHP/Depend/Code/Filter/PackageTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of PHP_Depend.
- * 
+ *
  * PHP Version 5
  *
  * Copyright (c) 2008, Manuel Pichler <mapi@pdepend.org>.
@@ -81,22 +81,22 @@ class PHP_Depend_Code_Filter_PackageTest extends PHP_Depend_AbstractTest
         $pkgIn2  = new PHP_Depend_Code_Package('in2');
         $pkgOut1 = new PHP_Depend_Code_Package('out1');
         $pkgOut2 = new PHP_Depend_Code_Package('out2');
-        
+
         $packages = array($pkgIn1, $pkgIn2, $pkgOut1, $pkgOut2);
         $iterator = new PHP_Depend_Code_NodeIterator($packages);
-        
+
         $filter = new PHP_Depend_Code_Filter_Package(array('out1', 'out2'));
         $iterator->addFilter($filter);
-        
+
         $expected = array('in1'  =>  true, 'in2'  =>  true);
-        
+
         foreach ($iterator as $pkg) {
             $this->assertArrayHasKey($pkg->getName(), $expected);
             unset($expected[$pkg->getName()]);
         }
         $this->assertEquals(0, count($expected));
     }
-    
+
     /**
      * Tests that the package filter works as expected on a class and interface
      * iterator.
@@ -108,26 +108,26 @@ class PHP_Depend_Code_Filter_PackageTest extends PHP_Depend_AbstractTest
         $pkgIn  = new PHP_Depend_Code_Package('in');
         $clsIn1 = $pkgIn->addType(new PHP_Depend_Code_Class('in1'));
         $clsIn2 = $pkgIn->addType(new PHP_Depend_Code_Interface('in2'));
-        
+
         $pkgOut  = new PHP_Depend_Code_Package('out');
         $clsOut1 = $pkgOut->addType(new PHP_Depend_Code_Class('out1'));
         $clsOut2 = $pkgOut->addType(new PHP_Depend_Code_Interface('out2'));
-        
+
         $classes  = array($clsIn1, $clsIn2, $clsOut1, $clsOut2);
         $iterator = new PHP_Depend_Code_NodeIterator($classes);
-        
+
         $filter = new PHP_Depend_Code_Filter_Package(array('out'));
         $iterator->addFilter($filter);
-        
+
         $expected = array('in1'  =>  true, 'in2'  =>  true);
-        
+
         foreach ($iterator as $cls) {
             $this->assertArrayHasKey($cls->getName(), $expected);
             unset($expected[$cls->getName()]);
         }
         $this->assertEquals(0, count($expected));
     }
-    
+
     /**
      * Tests the package filter for functions.
      *
@@ -138,53 +138,26 @@ class PHP_Depend_Code_Filter_PackageTest extends PHP_Depend_AbstractTest
         $pkgIn  = new PHP_Depend_Code_Package('in');
         $fcnIn1 = $pkgIn->addFunction(new PHP_Depend_Code_Function('in1'));
         $fcnIn2 = $pkgIn->addFunction(new PHP_Depend_Code_Function('in2'));
-        
+
         $pkgOut  = new PHP_Depend_Code_Package('out');
         $fcnOut1 = $pkgOut->addFunction(new PHP_Depend_Code_Function('out1'));
         $fcnOut2 = $pkgOut->addFunction(new PHP_Depend_Code_Function('out2'));
-        
+
         $functions = array($fcnIn1, $fcnIn2, $fcnOut1, $fcnOut2);
         $iterator  = new PHP_Depend_Code_NodeIterator($functions);
-        
+
         $filter = new PHP_Depend_Code_Filter_Package(array('out'));
         $iterator->addFilter($filter);
-        
+
         $expected = array('in1'  =>  true, 'in2'  =>  true);
-        
+
         foreach ($iterator as $fcn) {
             $this->assertArrayHasKey($fcn->getName(), $expected);
             unset($expected[$fcn->getName()]);
         }
         $this->assertEquals(0, count($expected));
     }
-    
-    public function testFilterMethodIterator()
-    {
-        $pkgIn  = new PHP_Depend_Code_Package('in');
-        $clsIn  = $pkgIn->addType(new PHP_Depend_Code_Class('in'));
-        $mtdIn1 = $clsIn->addMethod(new PHP_Depend_Code_Method('in1'));
-        $mtdIn2 = $clsIn->addMethod(new PHP_Depend_Code_Method('in2'));
-        
-        $pkgOut  = new PHP_Depend_Code_Package('out');
-        $clsOut  = $pkgOut->addType(new PHP_Depend_Code_Class('out'));
-        $mtdOut1 = $clsOut->addMethod(new PHP_Depend_Code_Method('out1'));
-        $mtdOut2 = $clsOut->addMethod(new PHP_Depend_Code_Method('out2'));
-        
-        $methods  = array($mtdIn1, $mtdIn2, $mtdOut1, $mtdOut2);
-        $iterator = new PHP_Depend_Code_NodeIterator($methods);
-        
-        $filter = new PHP_Depend_Code_Filter_Package(array('out'));
-        $iterator->addFilter($filter);
-        
-        $expected = array('in1'  =>  true, 'in2'  =>  true);
-        
-        foreach ($iterator as $mtd) {
-            $this->assertArrayHasKey($mtd->getName(), $expected);
-            unset($expected[$mtd->getName()]);
-        }
-        $this->assertEquals(0, count($expected));
-    }
-    
+
     /**
      * Tests that the package filter works with wild cards.
      *
@@ -196,15 +169,15 @@ class PHP_Depend_Code_Filter_PackageTest extends PHP_Depend_AbstractTest
         $pkgIn2  = new PHP_Depend_Code_Package('Zend_Controller');
         $pkgOut1 = new PHP_Depend_Code_Package('PHP_Depend_Code');
         $pkgOut2 = new PHP_Depend_Code_Package('PHP_Depend_Metrics');
-        
+
         $packages = array($pkgIn1, $pkgIn2, $pkgOut1, $pkgOut2);
         $iterator = new PHP_Depend_Code_NodeIterator($packages);
-        
+
         $filter = new PHP_Depend_Code_Filter_Package(array('ezc*', 'Zend_*'));
         $iterator->addFilter($filter);
-        
+
         $expected = array('PHP_Depend_Code'  =>  true, 'PHP_Depend_Metrics'  =>  true);
-        
+
         foreach ($iterator as $mtd) {
             $this->assertArrayHasKey($mtd->getName(), $expected);
             unset($expected[$mtd->getName()]);
