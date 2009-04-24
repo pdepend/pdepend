bc0a0ede916b629dbbf319f56a6d4c540743883c
Manuel Pichler <mapi@manuel-pichler.de>
2009-04-24 18:17:03 +0000

    # Code style issues fixed.
    git-svn-id: svn://xplib.de/PHP_Depend@812 b9671ce7-7109-4c08-962d-a4a26ea92831

--- branches/0.9.0/PHP/Depend/Util/UUID.php
+++ branches/0.9.0/PHP/Depend/Util/UUID.php
@@ -75,11 +75,13 @@ final class PHP_Depend_Util_UUID
      */
     public function __construct()
     {
-        $this->_uuid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        $this->_uuid = sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
             mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
             mt_rand(0, 0x0fff) | 0x4000,
             mt_rand(0, 0x3fff) | 0x8000,
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
     }
 
     /**
