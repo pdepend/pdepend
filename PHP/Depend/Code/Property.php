54f777bd9d1f4af87759e158cb2076789c8cd470
Manuel Pichler <mapi@manuel-pichler.de>
2009-05-23 15:34:07 +0000

    - Refs #67: PHP_Depend_Code_Property made compatible with ReflectionProperty.
    git-svn-id: svn://pdepend.org/PHP_Depend/branches/0.9.0@902 b9671ce7-7109-4c08-962d-a4a26ea92831

--- PHP/Depend/Code/Property.php
+++ PHP/Depend/Code/Property.php
@@ -60,8 +60,42 @@ require_once 'PHP/Depend/Code/AbstractItem.php';
  * @version    Release: @package_version@
  * @link       http://pdepend.org/
  */
-class PHP_Depend_Code_Property extends PHP_Depend_Code_AbstractItem
+class PHP_Depend_Code_Property 
+       extends ReflectionProperty
+    implements PHP_Depend_Code_NodeI
 {
+    /**
+     * The name for this item.
+     *
+     * @var string $_name
+     * @since 0.9.6
+     */
+    protected $_name = '';
+
+    /**
+     * The unique identifier for this function.
+     *
+     * @var PHP_Depend_Util_UUID $uuid
+     * @since 0.9.6
+     */
+    protected $uuid = null;
+
+    /**
+     * The source file for this item.
+     *
+     * @var PHP_Depend_Code_File $_sourceFile
+     * @since 0.9.6
+     */
+    protected $_sourceFile = null;
+
+    /**
+     * The comment for this type.
+     *
+     * @var string $_docComment
+     * @since 0.9.6
+     */
+    protected $_docComment = null;
+    
     /**
      * The parent type object.
      *
@@ -99,6 +133,49 @@ class PHP_Depend_Code_Property extends PHP_Depend_Code_AbstractItem
      * @since 0.9.6
      */
     private $_value = null;
+
+    /**
+     * Constructs a new item for the given <b>$name</b>.
+     *
+     * @param string $name The item name.
+     */
+    public function __construct($name)
+    {
+        $this->_name = $name;
+        $this->_uuid = new PHP_Depend_Util_UUID();
+    }
+
+    /**
+     * Returns the item name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    /**
+     * Returns a uuid for this code node.
+     *
+     * @return string
+     */
+    public function getUUID()
+    {
+        return (string) $this->_uuid;
+    }
+
+    /**
+     * This method returns a OR combined integer of the declared modifiers for
+     * this property.
+     *
+     * @return integer
+     * @since 0.9.6
+     */
+    public function getModifiers()
+    {
+        return $this->_modifiers;
+    }
 
     /**
      * This method sets a OR combined integer of the declared modifiers for this
@@ -233,6 +310,52 @@ class PHP_Depend_Code_Property extends PHP_Depend_Code_AbstractItem
     }
 
     /**
+     * Returns the source file for this item.
+     *
+     * @return PHP_Depend_Code_File
+     */
+    public function getSourceFile()
+    {
+        return $this->_sourceFile;
+    }
+
+    /**
+     * Sets the source file for this item.
+     *
+     * @param PHP_Depend_Code_File $sourceFile The item source file.
+     *
+     * @return void
+     */
+    public function setSourceFile(PHP_Depend_Code_File $sourceFile)
+    {
+        if ($this->_sourceFile === null || $this->_sourceFile->getName() === null) {
+            $this->_sourceFile = $sourceFile;
+        }
+    }
+
+    /**
+     * Returns the doc comment for this item or <b>null</b>.
+     *
+     * @return string
+     */
+    public function getDocComment()
+    {
+        return $this->_docComment;
+    }
+
+    /**
+     * Sets the doc comment for this item.
+     *
+     * @param string $docComment The doc comment block.
+     *
+     * @return void
+     */
+    public function setDocComment($docComment)
+    {
+        $this->_docComment = $docComment;
+    }
+
+    /**
      * Returns the source tokens used for this property declaration.
      *
      * @return array(PHP_Depend_Token)
@@ -283,13 +406,39 @@ class PHP_Depend_Code_Property extends PHP_Depend_Code_AbstractItem
     }
 
     /**
+     * This method will return the class where this property was declared.
+     *
+     * @return PHP_Depend_Code_AbstractClassOrInterface
+     * @since 0.9.6
+     */
+    public function getDeclaringClass()
+    {
+
+    }
+
+    /**
+     * This method will return <b>true</b> when the parameter declaration
+     * contains a default value.
+     *
+     * @return boolean
+     * @since 0.9.6
+     */
+    public function isDefaultValueAvailable()
+    {
+        if ($this->_value === null) {
+            return false;
+        }
+        return $this->_value->isValueAvailable();
+    }
+
+    /**
      * This method will return the default value for this property instance or
      * <b>null</b> when this property was only declared and not initialized.
      *
      * @return mixed
      * @since 0.9.6
      */
-    public function getValue()
+    public function getDefaultValue($object = null)
     {
         if ($this->_value === null) {
             return null;
@@ -301,14 +450,76 @@ class PHP_Depend_Code_Property extends PHP_Depend_Code_AbstractItem
      * This method is used by the parser to the a declared default value for
      * this property.
      *
-     * @param PHP_Depend_Code_Value $value The declared property default value.
+     * @param PHP_Depend_Code_Value $object The declared default value for this
+     *        property instance.
      *
      * @return void
      * @since 0.9.6
      */
-    public function setValue(PHP_Depend_Code_Value $value = null)
+    public function setDefaultValue(PHP_Depend_Code_Value $value)
     {
-        $this->_value = $value;
+        if ($this->_value === null) {
+            $this->_value = $value;
+        }
+    }
+
+    /**
+     * This method will return the value for this property instance on the given
+     * object or <b>null</b> when the property has no value.
+     *
+     * @param stdClass $object The context object for PHP's native reflection api.
+     *
+     * @return mixed
+     * @since 0.9.6
+     */
+    public function getValue($object = null)
+    {
+        throw new ReflectionException(__METHOD__ . '() is not supported.');
+    }
+
+    /**
+     * This method is used to modifiy the property value of the given object.
+     *
+     * @param stdClass $object The context class where this property should set
+     *                         the given value.
+     * @param mixed    $value  Default value used by the native Reflection api
+     *                         as new property default value.
+     *
+     * @return void
+     * @since 0.9.6
+     */
+    public function setValue($object, $value)
+    {
+        throw new ReflectionException(__METHOD__ . '() is not supported.');
+    }
+
+    /**
+     * This method can be used in PHP's native reflection api to allow access to
+     * private or protected object property. This userland implementation will
+     * always throw an exception.
+     *
+     * @param boolean $value Boolean <b>true</b> for accessible and <b>false</b>
+     *        for a write protected property.
+     *
+     * @return void
+     * @since 0.9.6
+     */
+    public function setAccessible($value)
+    {
+        throw new ReflectionException(__METHOD__ . '() is not supported.');
+    }
+
+    /**
+     * This method will return <b>true</b> when the context property was
+     * declared during compile time and not dynamically during runtime. In this
+     * reflection implementation the default value will be always <b>true</b>.
+     *
+     * @return boolean
+     * @since 0.9.6
+     */
+    public function isDefault()
+    {
+        return true;
     }
 
     /**
@@ -322,6 +533,39 @@ class PHP_Depend_Code_Property extends PHP_Depend_Code_AbstractItem
     public function accept(PHP_Depend_VisitorI $visitor)
     {
         $visitor->visitProperty($this);
+    }
+
+    /**
+     * This method returns a string representation of this parameter.
+     *
+     * @return string
+     * @since 0.9.6
+     */
+    public function __toString()
+    {
+        $default = ($this->isDefault() === true ? ' <default>' : '');
+        $static  = '';
+
+        if ($this->isStatic() === true) {
+            $default = '';
+            $static  = ' static';
+        }
+
+        $visibility = ' public';
+        if ($this->isProtected() === true) {
+            $visibility = ' protected';
+        } else if ($this->isPrivate() === true) {
+            $visibility = ' private';
+        }
+
+        return sprintf(
+            'Property [%s%s%s %s ]%s',
+            $default,
+            $visibility,
+            $static,
+            $this->_name,
+            PHP_EOL
+        );
     }
 
     // DEPRECATED METHODS
