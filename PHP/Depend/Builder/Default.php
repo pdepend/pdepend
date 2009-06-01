0d78d952e33b0aa0fb3924a84899c4015d7473de
Manuel Pichler <mapi@manuel-pichler.de>
2009-06-01 12:53:16 +0000

    - Refs #86: New ast class PHP_Depend_Code_FormalParameters added.
    git-svn-id: svn+ssh://xplib.de/var/svn/repos/PHP_Depend/branches/0.9.0@920 b9671ce7-7109-4c08-962d-a4a26ea92831

--- PHP/Depend/Builder/Default.php
+++ PHP/Depend/Builder/Default.php
@@ -535,6 +535,19 @@ class PHP_Depend_Builder_Default implements PHP_Depend_BuilderI
     }
 
     /**
+     * Builds a new formal parameters node.
+     *
+     * @return PHP_Depend_Code_FormalParameters
+     * @since 0.9.6
+     */
+    public function buildFormalParameters()
+    {
+        include_once 'PHP/Depend/Code/FormalParameters.php';
+
+        return new PHP_Depend_Code_FormalParameters();
+    }
+
+    /**
      * Returns an iterator with all generated {@link PHP_Depend_Code_Package}
      * objects.
      *
