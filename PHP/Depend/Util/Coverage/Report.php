f715b8f290d1e573597f990fd37e3d5c4df55003
Manuel Pichler <mapi@manuel-pichler.de>
2010-02-15 13:46:29 +0000

    - Implemented #74: Support for C.R.A.P. index implemented. The current   implementation uses clover compatible coverage reports as input format.
    git-svn-id: svn://xplib.de/PHP_Depend/branches/0.9.0@1059 b9671ce7-7109-4c08-962d-a4a26ea92831

--- PHP/Depend/Util/Coverage/Report.php
+++ PHP/Depend/Util/Coverage/Report.php
@@ -36,53 +36,36 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * @category  QualityAssurance
- * @package   PHP_Depend
- * @author    Manuel Pichler <mapi@pdepend.org>
- * @copyright 2008-2010 Manuel Pichler. All rights reserved.
- * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version   SVN: $Id$
- * @link      http://pdepend.org/
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage Util_Coverage
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2010 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://pdepend.org/
  */
-
-require_once 'PHPUnit/Framework.php';
-
-require_once dirname(__FILE__) . '/Coverage/AllTests.php';
-require_once dirname(__FILE__) . '/FileUtilTest.php';
-require_once dirname(__FILE__) . '/ImageConvertTest.php';
-require_once dirname(__FILE__) . '/MathUtilTest.php';
-require_once dirname(__FILE__) . '/TypeTest.php';
 
 /**
- * Main test suite for the PHP_Depend_Util package.
+ * Base interface representing a coverage report.
  *
- * @category  QualityAssurance
- * @package   PHP_Depend
- * @author    Manuel Pichler <mapi@pdepend.org>
- * @copyright 2008-2010 Manuel Pichler. All rights reserved.
- * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version   Release: @package_version@
- * @link      http://pdepend.org/
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage Util_Coverage
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2010 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://pdepend.org/
  */
-class PHP_Depend_Util_AllTests
+interface PHP_Depend_Util_Coverage_Report
 {
     /**
-     * Creates the phpunit test suite for this package.
+     * Returns the percentage code coverage for the given item instance.
      *
-     * @return PHPUnit_Framework_TestSuite
+     * @param PHP_Depend_Code_AbstractItem $item The context code item.
+     *
+     * @return float
      */
-    public static function suite()
-    {
-        $suite = new PHPUnit_Framework_TestSuite('PHP_Depend_Util - AllTests');
-
-        $suite->addTest(PHP_Depend_Util_Coverage_AllTests::suite());
-        
-        $suite->addTest(new PHP_Depend_Util_FileUtilTest());
-        $suite->addTest(new PHP_Depend_Util_MathUtilTest());
-
-        $suite->addTestSuite('PHP_Depend_Util_ImageConvertTest');
-        $suite->addTestSuite('PHP_Depend_Util_TypeTest');
-
-        return $suite;
-    }
+    function getCoverage(PHP_Depend_Code_AbstractItem $item);
 }
