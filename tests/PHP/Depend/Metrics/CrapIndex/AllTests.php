9ff655191f3fe88d94988d14ac886fa0c26da78c
Manuel Pichler <mapi@manuel-pichler.de>
2010-02-15 13:46:29 +0000

    - Implemented #74: Support for C.R.A.P. index implemented. The current   implementation uses clover compatible coverage reports as input format.
    git-svn-id: svn://pdepend.org/PHP_Depend/branches/0.9.0@1059 b9671ce7-7109-4c08-962d-a4a26ea92831

--- tests/PHP/Depend/Metrics/CrapIndex/AllTests.php
+++ tests/PHP/Depend/Metrics/CrapIndex/AllTests.php
@@ -36,53 +36,51 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * @category  QualityAssurance
- * @package   PHP_Depend
- * @author    Manuel Pichler <mapi@pdepend.org>
- * @copyright 2008-2010 Manuel Pichler. All rights reserved.
- * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version   SVN: $Id$
- * @link      http://pdepend.org/
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage Metrics_CrapIndex
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2010 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://pdepend.org/
  */
 
 require_once 'PHPUnit/Framework.php';
 
-require_once dirname(__FILE__) . '/Coverage/AllTests.php';
-require_once dirname(__FILE__) . '/FileUtilTest.php';
-require_once dirname(__FILE__) . '/ImageConvertTest.php';
-require_once dirname(__FILE__) . '/MathUtilTest.php';
-require_once dirname(__FILE__) . '/TypeTest.php';
+require_once dirname(__FILE__) . '/AnalyzerTest.php';
 
 /**
- * Main test suite for the PHP_Depend_Util package.
+ * Test suite for the Crap-Index subpackage.
  *
- * @category  QualityAssurance
- * @package   PHP_Depend
- * @author    Manuel Pichler <mapi@pdepend.org>
- * @copyright 2008-2010 Manuel Pichler. All rights reserved.
- * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version   Release: @package_version@
- * @link      http://pdepend.org/
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage Metrics_CrapIndex
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2010 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://pdepend.org/
  */
-class PHP_Depend_Util_AllTests
+class PHP_Depend_Metrics_CrapIndex_AllTests extends PHPUnit_Framework_TestSuite
 {
     /**
-     * Creates the phpunit test suite for this package.
+     * Constructs a new test suite instance.
+     */
+    public function __construct()
+    {
+        $this->setName('PHP::Depend::Metrics::CrapIndex::AllTests');
+
+        $this->addTestSuite('PHP_Depend_Metrics_CrapIndex_AnalyzerTest');
+    }
+
+    /**
+     * Returns a test suite instance.
      *
      * @return PHPUnit_Framework_TestSuite
      */
     public static function suite()
     {
-        $suite = new PHPUnit_Framework_TestSuite('PHP_Depend_Util - AllTests');
-
-        $suite->addTest(PHP_Depend_Util_Coverage_AllTests::suite());
-        
-        $suite->addTest(new PHP_Depend_Util_FileUtilTest());
-        $suite->addTest(new PHP_Depend_Util_MathUtilTest());
-
-        $suite->addTestSuite('PHP_Depend_Util_ImageConvertTest');
-        $suite->addTestSuite('PHP_Depend_Util_TypeTest');
-
-        return $suite;
+        return new PHP_Depend_Metrics_CrapIndex_AllTests();
     }
 }
---
+++ tests/PHP/Depend/Util/Coverage/FactoryTest.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * This file is part of PHP_Depend.
+ *
+ * PHP Version 5
+ *
+ * Copyright (c) 2008-2010, Manuel Pichler <mapi@pdepend.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage Util_Coverage
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2010 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://pdepend.org/
+ */
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+require_once 'PHP/Depend/Util/Coverage/Factory.php';
+
+/**
+ * Test case for the {@link PHP_Depend_Util_Coverage_Factory} class.
+ *
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage Util_Coverage
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2010 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://pdepend.org/
+ */
+class PHP_Depend_Util_Coverage_FactoryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * testCreateReturnsCloverReportInstanceForCloverInputFile
+     *
+     * @return void
+     * @covers PHP_Depend_Util_Coverage_Factory
+     * @group pdepend
+     * @group pdepend::util
+     * @group pdepend::util::coverage
+     * @group unittest
+     */
+    public function testCreateReturnsCloverReportInstanceForCloverInputFile()
+    {
+        $factory = new PHP_Depend_Util_Coverage_Factory();
+        $report  = $factory->create(dirname(__FILE__) . '/_files/clover.xml');
+
+        $this->assertType(PHP_Depend_Util_Coverage_CloverReport::TYPE, $report);
+    }
+
+    /**
+     * testCreateMethodThrowsExceptionWhenFileDoesNotExist
+     *
+     * @return void
+     * @covers PHP_Depend_Util_Coverage_Factory
+     * @group pdepend
+     * @group pdepend::util
+     * @group pdepend::util::coverage
+     * @group unittest
+     * @expectedException RuntimeException
+     */
+    public function testCreateMethodThrowsExceptionWhenFileDoesNotExist()
+    {
+        $factory = new PHP_Depend_Util_Coverage_Factory();
+        $factory->create(__FUNCTION__);
+    }
+
+    /**
+     * testCreateMethodThrowsExceptionWhenFileIsNotValidXml
+     *
+     * @return void
+     * @covers PHP_Depend_Util_Coverage_Factory
+     * @group pdepend
+     * @group pdepend::util
+     * @group pdepend::util::coverage
+     * @group unittest
+     * @expectedException RuntimeException
+     */
+    public function testCreateMethodThrowsExceptionWhenFileIsNotValidXml()
+    {
+        $factory = new PHP_Depend_Util_Coverage_Factory();
+        $factory->create(__FILE__);
+    }
+
+    /**
+     * testCreateMethodThrowsExceptionForUnsupportedReportFormat
+     *
+     * @return void
+     * @covers PHP_Depend_Util_Coverage_Factory
+     * @group pdepend
+     * @group pdepend::util
+     * @group pdepend::util::coverage
+     * @group unittest
+     * @expectedException RuntimeException
+     */
+    public function testCreateMethodThrowsExceptionForUnsupportedReportFormat()
+    {
+        $factory = new PHP_Depend_Util_Coverage_Factory();
+        $factory->create(dirname(__FILE__) . '/_files/fail.xml');
+    }
+}
