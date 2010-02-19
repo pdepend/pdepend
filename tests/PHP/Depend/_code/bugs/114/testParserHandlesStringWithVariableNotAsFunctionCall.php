3cdc2370ea44c65e821ecf45ada0f440b33aa0c1
Manuel Pichler <mapi@manuel-pichler.de>
2010-02-19 12:38:27 +0000

    - Refs #114: Some bugs in string parsing algorithm fixed.
    git-svn-id: svn://pdepend.org/PHP_Depend/branches/0.9.0@1081 b9671ce7-7109-4c08-962d-a4a26ea92831

---
+++ tests/PHP/Depend/_code/bugs/114/testParserHandlesStringWithVariableNotAsFunctionCall.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package xxx
+ */
+class test {
+	function __construct() {
+			"$fn($p1, '$user', '****', $p2)\n";
+			$s = "$dbms error: [$errno: $errmsg] in $fn($p1, $p2)\n";
+	}
+}
