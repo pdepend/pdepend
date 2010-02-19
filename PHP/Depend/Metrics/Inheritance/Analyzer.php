5121455dfe4a27e4eca030d2741801f08a21b8e6
Manuel Pichler <mapi@manuel-pichler.de>
2010-02-19 12:38:19 +0000

    - Refs #72: Support for new metrics started.
    git-svn-id: svn://pdepend.org/PHP_Depend/branches/0.9.0@1080 b9671ce7-7109-4c08-962d-a4a26ea92831

--- PHP/Depend/Metrics/Inheritance/Analyzer.php
+++ PHP/Depend/Metrics/Inheritance/Analyzer.php
@@ -47,6 +47,7 @@
  */
 
 require_once 'PHP/Depend/Metrics/AbstractAnalyzer.php';
+require_once 'PHP/Depend/Metrics/NodeAwareI.php';
 require_once 'PHP/Depend/Metrics/FilterAwareI.php';
 require_once 'PHP/Depend/Metrics/ProjectAwareI.php';
 
@@ -71,7 +72,8 @@ require_once 'PHP/Depend/Metrics/ProjectAwareI.php';
  */
 class PHP_Depend_Metrics_Inheritance_Analyzer
        extends PHP_Depend_Metrics_AbstractAnalyzer
-    implements PHP_Depend_Metrics_FilterAwareI,
+    implements PHP_Depend_Metrics_NodeAwareI,
+               PHP_Depend_Metrics_FilterAwareI,
                PHP_Depend_Metrics_ProjectAwareI
 {
     /**
@@ -83,7 +85,11 @@ class PHP_Depend_Metrics_Inheritance_Analyzer
      * Metrics provided by the analyzer implementation.
      */
     const M_AVERAGE_NUMBER_DERIVED_CLASSES = 'andc',
-          M_AVERAGE_HIERARCHY_HEIGHT       = 'ahh';
+          M_AVERAGE_HIERARCHY_HEIGHT       = 'ahh',
+          M_DEPTH_OF_INHERITANCE_TREE      = 'dit',
+          M_NUMBER_OF_ADDED_METHODS        = 'noam',
+          M_NUMBER_OF_OVERWRITTEN_METHODS  = 'noom',
+          M_NUMBER_OF_DERIVED_CLASSES      = 'nocc';
 
     /**
      * Contains the number of derived classes for each processed class. The array
@@ -98,23 +104,51 @@ class PHP_Depend_Metrics_Inheritance_Analyzer
      * analyzed system. The array size is equal to the number of analyzed root
      * classes.
      *
-     * @var array(integer) $_rootClasses
+     * @var array(integer)
      */
     private $_rootClasses = null;
 
     /**
      * The average number of derived classes.
      *
-     * @var float $_andc
+     * @var float
      */
     private $_andc = 0;
 
     /**
      * The average hierarchy height.
      *
-     * @var float $_ahh
+     * @var float
      */
     private $_ahh = 0;
+
+    private $_numberOfClasses = 0;
+
+    private $_numberOfDerivedClasses = 0;
+
+    /**
+     * Metrics calculated for a single source node.
+     *
+     * @var array(string=>array)
+     */
+    private $_nodeMetrics = array();
+
+    /**
+     * This method will return an <b>array</b> with all generated metric values
+     * for the given <b>$node</b>. If there are no metrics for the requested
+     * node, this method will return an empty <b>array</b>.
+     *
+     * @param PHP_Depend_Code_NodeI $node The context node instance.
+     *
+     * @return array(string=>mixed)
+     */
+    public function getNodeMetrics(PHP_Depend_Code_NodeI $node)
+    {
+        if (isset($this->_nodeMetrics[$node->getUUID()])) {
+            return $this->_nodeMetrics[$node->getUUID()];
+        }
+        return array();
+    }
 
     /**
      * Provides the project summary as an <b>array</b>.
@@ -157,8 +191,8 @@ class PHP_Depend_Metrics_Inheritance_Analyzer
                 $package->accept($this);
             }
 
-            if (($count = count($this->_derivedClasses)) > 0) {
-                $this->_andc = (array_sum($this->_derivedClasses) / $count);
+            if ($this->_numberOfClasses > 0) {
+                $this->_andc = ($this->_numberOfDerivedClasses / $this->_numberOfClasses);
             }
             if (($count = count($this->_rootClasses)) > 0) {
                 $this->_ahh = (array_sum($this->_rootClasses) / $count);
@@ -179,8 +213,10 @@ class PHP_Depend_Metrics_Inheritance_Analyzer
     public function visitClass(PHP_Depend_Code_Class $class)
     {
         $this->fireStartClass($class);
+
+        $this->_initNodeMetricsForClass($class);
         
-        $this->_calculateNODC($class);
+        $this->_calculateNumberOfDerivedClasses($class);
         $this->_calculateHIT($class);
 
         $this->fireEndClass($class);
@@ -194,7 +230,7 @@ class PHP_Depend_Metrics_Inheritance_Analyzer
      * @return void
      * @since 0.9.5
      */
-    private function _calculateNODC(PHP_Depend_Code_Class $class)
+    private function _calculateNumberOfDerivedClasses(PHP_Depend_Code_Class $class)
     {
         $uuid = $class->getUUID();
         if (isset($this->_derivedClasses[$uuid]) === false) {
@@ -204,10 +240,9 @@ class PHP_Depend_Metrics_Inheritance_Analyzer
         $parentClass = $class->getParentClass();
         if ($parentClass !== null) {
             $uuid = $parentClass->getUUID();
-            if (isset($this->_derivedClasses[$uuid]) === false) {
-                $this->_derivedClasses[$uuid] = 0;
-            }
-            ++$this->_derivedClasses[$uuid];
+
+            ++$this->_numberOfDerivedClasses;
+            ++$this->_nodeMetrics[$uuid][self::M_NUMBER_OF_DERIVED_CLASSES];
         }
     }
 
@@ -220,20 +255,47 @@ class PHP_Depend_Metrics_Inheritance_Analyzer
      */
     private function _calculateHIT(PHP_Depend_Code_Class $class)
     {
-        $uuid = $class->getUUID();
         $dit  = 0;
+        $uuid = $class->getUUID();
+        $root = $class->getUUID();
 
-        $parent = $class->getParentClass();
-        while ($parent !== null) {
+        while (($class = $class->getParentClass()) !== null) {
             ++$dit;
-            $uuid   = $parent->getUUID();
-            $parent = $parent->getParentClass();
+            $root = $class->getUUID();
         }
 
-        if (isset($this->_rootClasses[$uuid]) === false 
-            || $this->_rootClasses[$uuid] < $dit
-        ) {
-            $this->_rootClasses[$uuid] = $dit;
+        if (empty($this->_rootClasses[$root]) || $this->_rootClasses[$root] < $dit) {
+            $this->_rootClasses[$root] = $dit;
         }
+        $this->_nodeMetrics[$uuid][self::M_DEPTH_OF_INHERITANCE_TREE] = $dit;
+    }
+
+    private function _calculateNumberOfAddedMethods(PHP_Depend_Code_Class $class)
+    {
+        $parentClass = $class->getParentClass();
+        if ($parentClass === null) {
+            return;
+        }
+    }
+
+    private function _initNodeMetricsForClass(PHP_Depend_Code_Class $class)
+    {
+        if (is_object($class->getParentClass())) {
+            $this->_initNodeMetricsForClass($class->getParentClass());
+        }
+
+        $uuid = $class->getUUID();
+        if (isset($this->_nodeMetrics[$uuid])) {
+            return;
+        }
+
+        ++$this->_numberOfClasses;
+        
+        $this->_nodeMetrics[$uuid] = array(
+            self::M_DEPTH_OF_INHERITANCE_TREE     => 0,
+            self::M_NUMBER_OF_ADDED_METHODS       => 0,
+            self::M_NUMBER_OF_DERIVED_CLASSES     => 0,
+            self::M_NUMBER_OF_OVERWRITTEN_METHODS => 0
+        );
     }
 }
