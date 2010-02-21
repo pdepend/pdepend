d420695c604c24bf04506572360b42b75cb6a3f1
Manuel Pichler <mapi@manuel-pichler.de>
2010-02-21 12:55:18 +0000

    - Refs #72: Number Of Child Classes metric calculation tested.
    git-svn-id: svn://pdepend.org/PHP_Depend/branches/0.9.0@1083 b9671ce7-7109-4c08-962d-a4a26ea92831

---
+++ tests/PHP/Depend/_code/metrics/Inheritance/testCalculatesExpectedNoccMetricForClassWithDirectChildren.php
@@ -0,0 +1,9 @@
+<?php
+class testCalculatesExpectedNoccMetricForClassWithDirectChildren
+{
+
+}
+
+class c1 extends testCalculatesExpectedNoccMetricForClassWithDirectChildren {}
+class c2 extends testCalculatesExpectedNoccMetricForClassWithDirectChildren {}
+class c3 extends testCalculatesExpectedNoccMetricForClassWithDirectChildren {}
