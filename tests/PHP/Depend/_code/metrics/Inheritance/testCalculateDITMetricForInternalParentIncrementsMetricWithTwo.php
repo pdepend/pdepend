7c419855632e5a1a37e74f3543cf2b42d3f3d539
Manuel Pichler <mapi@manuel-pichler.de>
2010-02-23 16:57:46 +0000

    - Refs #115: Additional tests for dit calculation added.
    git-svn-id: svn+ssh://xplib.de/var/svn/repos/PHP_Depend/branches/0.9.0@1098 b9671ce7-7109-4c08-962d-a4a26ea92831

---
+++ tests/PHP/Depend/_code/metrics/Inheritance/testCalculateDITMetricForInternalParentIncrementsMetricWithTwo.php
@@ -0,0 +1,10 @@
+<?php
+class testCalculateDITMetricForInternalParentIncrementsMetricWithTwo extends testCalculateDITMetricForInternalParentIncrementsMetricWithTwoParent
+{
+
+}
+
+class testCalculateDITMetricForInternalParentIncrementsMetricWithTwoParent extends ArrayObject
+{
+    
+}
