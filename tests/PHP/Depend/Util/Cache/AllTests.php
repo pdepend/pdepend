1d4da9662d254a24d8b2a5e462a50526b03fb92a
Manuel Pichler <mapi@manuel-pichler.de>
2010-02-28 12:09:39 +0000

    Merge branch 'master' into HEAD
    Conflicts:
    	PHP/Depend/Storage/FileEngine.php
    
    git-svn-id: svn+ssh://xplib.de/var/svn/repos/PHP_Depend/branches/0.9.0@1109 b9671ce7-7109-4c08-962d-a4a26ea92831

--- tests/PHP/Depend/Util/Cache/AllTests.php
+++ tests/PHP/Depend/Util/Cache/AllTests.php
@@ -36,36 +36,33 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * @category  QualityAssurance
- * @package   PHP_Depend
- * @author    Manuel Pichler <mapi@pdepend.org>
- * @copyright 2008-2010 Manuel Pichler. All rights reserved.
- * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version   SVN: $Id$
- * @link      http://pdepend.org/
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage Util_Cache
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2010 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php BSD License
+ * @version    SVN: $Id$
+ * @link       http://pdepend.org/
  */
 
 require_once 'PHPUnit/Framework.php';
 
-require_once dirname(__FILE__) . '/Cache/AllTests.php';
-require_once dirname(__FILE__) . '/Coverage/AllTests.php';
-require_once dirname(__FILE__) . '/FileUtilTest.php';
-require_once dirname(__FILE__) . '/ImageConvertTest.php';
-require_once dirname(__FILE__) . '/MathUtilTest.php';
-require_once dirname(__FILE__) . '/TypeTest.php';
+require_once dirname(__FILE__) . '/FileEngineTest.php';
 
 /**
- * Main test suite for the PHP_Depend_Util package.
+ * Main test suite for the PHP_Depend_Util_Cache package.
  *
- * @category  QualityAssurance
- * @package   PHP_Depend
- * @author    Manuel Pichler <mapi@pdepend.org>
- * @copyright 2008-2010 Manuel Pichler. All rights reserved.
- * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version   Release: @package_version@
- * @link      http://pdepend.org/
+ * @category   QualityAssurance
+ * @package    Util_Cache
+ * @subpackage Util_Coverage
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2010 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php BSD License
+ * @version    Release: @package_version@
+ * @link       http://pdepend.org/
  */
-class PHP_Depend_Util_AllTests
+class PHP_Depend_Util_Cache_AllTests
 {
     /**
      * Creates the phpunit test suite for this package.
@@ -74,16 +71,9 @@ class PHP_Depend_Util_AllTests
      */
     public static function suite()
     {
-        $suite = new PHPUnit_Framework_TestSuite('PHP_Depend_Util - AllTests');
+        $suite = new PHPUnit_Framework_TestSuite('PHP_Depend_Util_Cache - AllTests');
 
-        $suite->addTest(PHP_Depend_Util_Cache_AllTests::suite());
-        $suite->addTest(PHP_Depend_Util_Coverage_AllTests::suite());
-
-        $suite->addTest(new PHP_Depend_Util_FileUtilTest());
-        $suite->addTest(new PHP_Depend_Util_MathUtilTest());
-
-        $suite->addTestSuite('PHP_Depend_Util_ImageConvertTest');
-        $suite->addTestSuite('PHP_Depend_Util_TypeTest');
+        $suite->addTestSuite('PHP_Depend_Util_Cache_FileEngineTest');
 
         return $suite;
     }
