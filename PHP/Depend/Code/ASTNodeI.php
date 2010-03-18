cb24c7ad8797e48c4cbdcf1aa087336c80cac850
Manuel Pichler <mapi@manuel-pichler.de>
2010-03-18 13:46:11 +0000

    # Missing documentation blocks added.
    git-svn-id: svn://xplib.de/PHP_Depend/branches/0.9.0@1187 b9671ce7-7109-4c08-962d-a4a26ea92831

--- PHP/Depend/Code/ASTNodeI.php
+++ PHP/Depend/Code/ASTNodeI.php
@@ -186,5 +186,14 @@ interface PHP_Depend_Code_ASTNodeI
      */
     function accept(PHP_Depend_Code_ASTVisitorI $visitor, $data = null);
 
+    /**
+     * This method can be called by the PHP_Depend runtime environment or a
+     * utilizing component to free up memory. This methods are required for
+     * PHP version < 5.3 where cyclic references can not be resolved
+     * automatically by PHP's garbage collector.
+     *
+     * @return void
+     * @since 0.9.12
+     */
     function free();
 }
