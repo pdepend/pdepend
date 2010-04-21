299bafe7af694c915efc1a1042b6209cd187e05b
Manuel Pichler <mapi@manuel-pichler.de>
2010-04-21 22:18:13 +0200

    # Test cases fixed.

--- tests/PHP/Depend/_code/code/ASTIfStatement/testHasElseMethodReturnsFalseWhenElseBranchWithIfExists.php
+++
@@ -1,9 +0,0 @@
-<?php
-function testHasElseMethodReturnsFalseWhenElseBranchWithIfExists()
-{
-    if (func_num_args() === 0) {
-
-    } else if (func_num_args() === 1) {
-
-    }
-}
--- tests/PHP/Depend/_code/code/ASTIfStatement/testHasElseMethodReturnsFalseWhenElseIfBranchExists.php
+++
@@ -1,9 +0,0 @@
-<?php
-function testHasElseMethodReturnsFalseWhenElseIfBranchExists()
-{
-    if (func_num_args() === 0) {
-
-    } elseif (func_num_args() === 1) {
-
-    }
-}
