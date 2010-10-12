eee3c16b8532bda30ff9885ed2be18789a639e7c
Manuel Pichler <mapi@manuel-pichler.de>
2010-10-12 23:02:01 +0200

    # Separate concerns, IO and validation related code.

--- PHP/Depend/Util/Cache/Driver/File.php
+++ PHP/Depend/Util/Cache/Driver/File.php
@@ -14,7 +14,16 @@ class PHP_Depend_Util_Cache_Driver_File implements PHP_Depend_Util_Cache_Driver
     public function store($key, $data, $hash = null)
     {
         $file = "{$this->cacheDir}/{$key}.cache";
-        file_put_contents($file, serialize(array('hash' => $hash, 'data' => $data)));
+        $this->write($file, serialize(array('hash' => $hash, 'data' => $data)));
+    }
+
+    protected function write($file, $data)
+    {
+        $handle = fopen($file, 'wb');
+        flock($handle, LOCK_EX);
+        fwrite($handle, $data);
+        flock($handle, LOCK_UN);
+        fclose($handle);
     }
 
     public function restore($key, $hash = null)
@@ -28,10 +37,23 @@ class PHP_Depend_Util_Cache_Driver_File implements PHP_Depend_Util_Cache_Driver
 
     protected function restoreFile($file, $hash)
     {
-        $data = unserialize(file_get_contents($file));
+        $data = unserialize($this->read($file));
         if ($data['hash'] === $hash) {
             return $data['data'];
         }
         return null;
+    }
+
+    protected function read($file)
+    {
+        $handle = fopen($file, 'rb');
+        flock($handle, LOCK_EX);
+
+        $data = fread($handle, filesize($file));
+
+        flock($handle, LOCK_UN);
+        fclose($handle);
+
+        return $data;
     }
 }
