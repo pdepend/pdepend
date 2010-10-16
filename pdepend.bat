01edcc0fa5533b1dd3c0e1c315fbd8c4366471c5
Manuel Pichler <mapi@manuel-pichler.de>
2010-10-16 11:48:58 +0000

    # Batch file fixed, based on commit PHPUnit commit 32b7c5d813b6401d0e18.
    git-svn-id: svn+ssh://xplib.de/var/svn/repos/PHP_Depend/trunk@1537 b9671ce7-7109-4c08-962d-a4a26ea92831

--- pdepend.bat
+++ pdepend.bat
@@ -36,5 +36,11 @@ REM
 REM $Id$
 REM
 
-set PHPBIN="@php_bin@"
-"@php_bin@" "@bin_dir@\pdepend" %*
+
+if "%PHPBIN%" == "" set PHPBIN=@php_bin@
+if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
+GOTO RUN
+:USE_PEAR_PATH
+set PHPBIN=%PHP_PEAR_PHP_BIN%
+:RUN
+"%PHPBIN%" "@bin_dir@\pdepend" %*
