716069f148f337d6c27837768bc0947722964e5f
Manuel Pichler <mapi@manuel-pichler.de>
2010-10-30 00:05:22 +0200

    # Magic sleep method isPPP of field declaration class tested.

--- PHP/Depend/Code/ASTFieldDeclaration.php
+++ PHP/Depend/Code/ASTFieldDeclaration.php
@@ -205,17 +205,16 @@ class PHP_Depend_Code_ASTFieldDeclaration extends PHP_Depend_Code_ASTNode
         return $visitor->visitFieldDeclaration($this, $data);
     }
 
+    /**
+     * The magic sleep method will be called by PHP's runtime environment right
+     * before an instance of this class gets serialized. It should return an
+     * array with those property names that should be serialized for this class.
+     *
+     * @return array(string)
+     * @since 0.10.0
+     */
     public function  __sleep()
     {
-        return array(
-            'image',
-            'comment',
-            'startLine',
-            'startColumn',
-            'endLine',
-            'endColumn',
-            'nodes',
-            'modifiers'
-        );
+        return array_merge(array('modifiers'), parent::__sleep());
     }
 }
