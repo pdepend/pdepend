931b6f8e165cb2b1613a87d92140afdaead35d5e
Manuel Pichler <mapi@manuel-pichler.de>
2011-10-17 22:34:07 +0200

    Naming unified.

--- src/test/resources/files/Code/ASTFunctionPostfix/testFunctionPostfixStructureWithMemberPrimaryPrefixProperty.php
+++
@@ -1,8 +0,0 @@
-<?php
-function testFunctionPostfixStructureWithMemberPrimaryPrefixProperty($i = 0)
-{
-    if ($i === 0) {
-        testFunctionPostfixStructureWithMemberPrimaryPrefixProperty(1)->bar;
-    }
-    return new stdClass();
-}
