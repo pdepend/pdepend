5d6722a08ccccde80558a57ea3fce9d232863bec
Manuel Pichler <mapi@manuel-pichler.de>
2012-02-02 12:00:48 +0100

    Fixed: Log file names.

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -2,7 +2,7 @@
 
 rm -rf ~/.pdepend/*
 pdepend --summary-xml=/tmp/sum01.xml $1
-pdepend --summary-xml=/tmp/sum01.xml $1
+pdepend --summary-xml=/tmp/sum01c.xml $1
 src/bin/pdepend.php --summary-xml=/tmp/sum02.xml $1
 src/bin/pdepend.php --summary-xml=/tmp/sum03.xml $1
 meld /tmp/sum01.xml /tmp/sum02.xml /tmp/sum03.xml 
