23ffa2dd12408da58da7a585ba0f735ac9aaea5b
Manuel Pichler <mapi@manuel-pichler.de>
2012-02-14 20:58:02 +0100

    Implemented: Allow different PHP versions.

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+PHP_BIN=${2:-"php"}
+
 rm -rf ~/.pdepend/*
-pdepend --jdepend-xml=/tmp/jdpa0.xml --summary-xml=/tmp/suma0.xml $1
-pdepend --jdepend-xml=/tmp/jdpa1.xml --summary-xml=/tmp/suma1.xml $1
-src/bin/pdepend.php --jdepend-xml=/tmp/jdpb0.xml --summary-xml=/tmp/sumb0.xml $1
-src/bin/pdepend.php --jdepend-xml=/tmp/jdpb1.xml --summary-xml=/tmp/sumb1.xml $1
+$PHP_BIN `which pdepend` --jdepend-xml=/tmp/jdpa0.xml --summary-xml=/tmp/suma0.xml $1
+$PHP_BIN `which pdepend` --jdepend-xml=/tmp/jdpa1.xml --summary-xml=/tmp/suma1.xml $1
+$PHP_BIN src/bin/pdepend.php --jdepend-xml=/tmp/jdpb0.xml --summary-xml=/tmp/sumb0.xml $1
+$PHP_BIN src/bin/pdepend.php --jdepend-xml=/tmp/jdpb1.xml --summary-xml=/tmp/sumb1.xml $1
 
 meld /tmp/suma0.xml /tmp/sumb0.xml /tmp/sumb1.xml
 
