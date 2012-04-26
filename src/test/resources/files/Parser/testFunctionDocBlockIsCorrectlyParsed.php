24efdf68df102201a98eb70f64d11c1694ef64a8
Slashbunny <demodevil@gmail.com>
2012-04-26 11:11:30 -0400

    Added function doc block comment test for Parser
    Under some scenarios, the Parser pulls a random comment
    from the function body and sets it as the DocComment. In
    this test, the Parser incorrectly returns: "// Function
    Comment 3" when getDocComment() is called.

---
+++ src/test/resources/files/Parser/testFunctionDocBlockIsCorrectlyParsed.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This is the function docblock for foo
+ *
+ */
+function foo() {
+    // Function Comment 1
+    $a = 1;
+
+    // Function Comment 2
+    if ( $a == 1 ) {
+        echo 1;
+    }
+
+    // Function Comment 3
+    echo $a;
+}
+
