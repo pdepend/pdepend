91c1fb78b94d7f7089f9638b1d0f8d242044966d
Flyingmana <flyingmana@googlemail.com>
2013-12-02 20:54:13 +0100

    add testcase for issue #124
    also add the new needed Token

--- src/main/php/PDepend/Source/Tokenizer/Tokens.php
+++ src/main/php/PDepend/Source/Tokenizer/Tokens.php
@@ -836,6 +836,11 @@ interface Tokens
     const T_INSTEADOF = 157;
 
     /**
+     * Token that represents the stdClass::class and $class::class constant.
+     */
+    const T_CLASS_FQN_CONSTANT = 158;
+
+    /**
      * Marks any content not between php tags.
      */
     const T_NO_PHP = 255;
