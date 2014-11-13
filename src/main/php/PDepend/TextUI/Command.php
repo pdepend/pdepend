3787e7822b2724473736d8ba0e12a70a77205f42
Tomas Prochazka <kacer@bizbox.cz>
2014-11-13 15:21:51 +0100

    Fixed display of duration longer than one hour

--- src/main/php/PDepend/TextUI/Command.php
+++ src/main/php/PDepend/TextUI/Command.php
@@ -248,7 +248,11 @@ class Command
                 echo PHP_EOL;
             }
 
-            echo PHP_EOL, 'Time: ', date('i:s', time() - $startTime);
+            $duration = time() - $startTime;
+            $hours = intval($duration / 3600);
+            $minutes = intval(($duration - $hours * 3600) / 60);
+            $seconds = $duration % 60;
+            echo PHP_EOL, 'Time: ', sprintf('%d:%02d:%02d', $hours, $minutes, $seconds);
             if (function_exists('memory_get_peak_usage')) {
                 $memory = (memory_get_peak_usage(true) / (1024 * 1024));
                 printf('; Memory: %4.2fMb', $memory);
