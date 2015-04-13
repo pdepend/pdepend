fa7707e8f6dd31eb5509ad7723953b9a2eae46b5
Matthias Mullie <git@mullie.eu>
2015-04-13 09:09:05 +0200

    Add tests for Maintainability Index analyzer

---
+++ src/test/resources/files/Metrics/Analyzer/MaintainabilityIndexAnalyzer/testAnalyzerRestoresExpectedMethodMetricsFromCache.php
@@ -0,0 +1,20 @@
+<?php
+interface MIMethodInterface {
+    function pdepend1($x);
+    function pdepend2($x);
+}
+
+
+class MIMethodClass implements MIMethodInterface
+{
+    function pdepend1($x)
+    {
+        $value = $x + 1;
+        return $value;
+    }
+
+    function pdepend2($x)
+    {
+        return $this->pdepend1( $x );
+    }
+}
