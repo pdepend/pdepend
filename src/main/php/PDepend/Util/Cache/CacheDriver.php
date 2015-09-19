a5810b605a4fd0b17ada6999f64a2baadec9b0a6
Manuel Pichler <github@manuel-pichler.de>
2015-09-19 12:22:04 +0200

    Fixes #213: Missing constants added for backward compatibility

--- src/main/php/PDepend/Util/Cache/CacheDriver.php
+++ src/main/php/PDepend/Util/Cache/CacheDriver.php
@@ -55,7 +55,7 @@ interface CacheDriver
     /**
      * The current cache version.
      */
-    const VERSION = '@version:8c0e76329bd5807342f3274c5d9e6262:@';
+    const VERSION = '@version:fe3bfe797bb91807acf9cf55cfe115cf:@';
 
     /**
      * Sets the type for the next <em>store()</em> or <em>restore()</em> method
