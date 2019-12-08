0979aef74072bc4ee57205066e318c8423b704b2
Kyle <kylekatarnls@users.noreply.github.com>
2019-12-08 11:32:18 +0100

    Fix PHP class name in @link test reference

--- src/test/php/PDepend/Source/Language/PHP/PHPParserVersion72Test.php
+++ src/test/php/PDepend/Source/Language/PHP/PHPParserVersion72Test.php
@@ -47,7 +47,7 @@ use PDepend\Source\AST\ASTMethod;
 use PDepend\Source\AST\ASTParameter;
 
 /**
- * Test case for the {@link \PDepend\Source\Language\PHP\PHPParserVersion71} class.
+ * Test case for the {@link \PDepend\Source\Language\PHP\PHPParserVersion72} class.
  *
  * @copyright 2008-2017 Manuel Pichler. All rights reserved.
  * @license http://www.opensource.org/licenses/bsd-license.php BSD License
