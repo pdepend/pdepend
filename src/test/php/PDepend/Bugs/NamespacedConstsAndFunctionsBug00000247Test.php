358a33b2485d5efb3bb14dbdc62558a87e0da875
Tobias van Beek <t.vanbeek@tjvb.nl>
2023-12-15 19:38:27 +0100

    Update NamespacedConstsAndFunctionsBug00000247Test to rever an existing Parser.

--- src/test/php/PDepend/Bugs/NamespacedConstsAndFunctionsBug00000247Test.php
+++ src/test/php/PDepend/Bugs/NamespacedConstsAndFunctionsBug00000247Test.php
@@ -117,7 +117,7 @@ class NamespacedConstsAndFunctionsBug00000247Test extends AbstractRegressionTest
     protected function createPHPParser(Tokenizer $tokenizer, Builder $builder, CacheDriver $cache)
     {
         return $this->getAbstractClassMock(
-            'PDepend\\Source\\Language\\PHP\\PHPParserVersion56',
+            'PDepend\\Source\\Language\\PHP\\PHPParserVersion70',
             array($tokenizer, $builder, $cache)
         );
     }
